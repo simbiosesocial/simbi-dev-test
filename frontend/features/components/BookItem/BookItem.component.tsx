be3f8e619c6715940df7864515516dba4551fbfe
Thiago Santa Clara <strovsk@outlook.com>
2024-08-24 20:06:40 -0300

    feat: add loan book feature

--- frontend/features/components/BookItem/BookItem.component.tsx
+++ frontend/features/components/BookItem/BookItem.component.tsx
@@ -1,8 +1,31 @@
-import type { FunctionComponent } from "react";
+import { useState, type FunctionComponent } from "react";
 import type { BookItemProps } from "./BookItem.interface";
 import { Card, CardContent, CardMedia, CardActions, Button, Typography, Tooltip } from "@mui/material";
+import loanBook from "@/services/loanBook";
+import dateObjToApiFormat from "@/common/utils/dateObjToApiFormat";
 
-export const BookItem: FunctionComponent<BookItemProps> = ({ title, coverUrl }) => {
+export const BookItem: FunctionComponent<BookItemProps> = ({ title, coverUrl, id }) => {
+  const [loanProcess, setLoanProces] = useState<{isLoaning: boolean, message: string}>({isLoaning: false, message: "Aguarde"});
+
+  const handleLoanBookAction = async (bookId: string) => {
+    setLoanProces({isLoaning: true, message: "Emprestando Livro"});
+    console.log('clicked');
+    const today = new Date();
+    const todayFormatted = dateObjToApiFormat(today);
+
+    const eightDaysLater = new Date();
+    eightDaysLater.setDate(today.getDate() + 8);
+    const eightDaysLaterFormatted = dateObjToApiFormat(eightDaysLater);
+
+    try {
+      await loanBook(bookId, todayFormatted, eightDaysLaterFormatted);
+      setLoanProces({isLoaning: true, message: "Livro Emprestado! 😀"})
+    } catch (error) {
+      setLoanProces({isLoaning: true, message: "Occorreu Um Erro 😔. Talvez já tenha sido emprestado"});
+    }
+    setTimeout(() => setLoanProces({isLoaning: false, message: "Aguarde"}), 1500);
+  };
+
   return (
     <Card variant="outlined">
       <CardMedia sx={{ height: 264 }} image={coverUrl ? coverUrl : "/cover.png"} title={title} />
@@ -15,9 +38,12 @@ export const BookItem: FunctionComponent<BookItemProps> = ({ title, coverUrl }) 
         <Typography variant="body2" color="text.secondary">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam non arcu...
         </Typography>
+        {
+          loanProcess.isLoaning && <Typography variant="body2">{loanProcess.message}</Typography>
+        }
       </CardContent>
       <CardActions>
-        <Button size="small" variant="contained" fullWidth>
+        <Button size="small" variant="contained" fullWidth onClick={ () => handleLoanBookAction(id) } disabled={ loanProcess.isLoaning }>
           Empréstimo
         </Button>
       </CardActions>
