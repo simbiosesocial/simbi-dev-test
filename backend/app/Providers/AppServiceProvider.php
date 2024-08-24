d28189213892240ac6a00df2640e5298b3a0d7d5
Thiago Santa Clara <strovsk@outlook.com>
2024-08-24 09:29:39 -0300

    chore: bind list all loans controller to the container pointer

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -6,6 +6,7 @@ use App\Adapters\Presenters\Library\CreateAuthorJsonPresenter;
 use App\Adapters\Presenters\Library\CreateBookJsonPresenter;
 use App\Adapters\Presenters\Library\CreateLoanJsonPresenter;
 use App\Adapters\Presenters\Library\ListAllBooksJsonPresenter;
+use App\Adapters\Presenters\Library\ListAllLoansJsonPresenter;
 use App\Adapters\Presenters\Library\ListBooksByAuthorJsonPresenter;
 use App\Core\Domain\Library\Ports\Persistence\AuthorRepository;
 use App\Core\Domain\Library\Ports\Persistence\BookRepository;
@@ -14,16 +15,19 @@ use App\Core\Domain\Library\Ports\UseCases\CreateAuthor\CreateAuthorUseCase;
 use App\Core\Domain\Library\Ports\UseCases\CreateBook\CreateBookUseCase;
 use App\Core\Domain\Library\Ports\UseCases\CreateLoan\CreateLoanUseCase;
 use App\Core\Domain\Library\Ports\UseCases\ListAllBooks\ListAllBooksUseCase;
+use App\Core\Domain\Library\Ports\UseCases\ListAllLoans\ListAllLoansUseCase;
 use App\Core\Domain\Library\Ports\UseCases\ListBooksByAuthor\ListBooksByAuthorUseCase;
 use App\Core\Services\Library\CreateAuthorService;
 use App\Core\Services\Library\CreateBookService;
 use App\Core\Services\Library\CreateLoanService;
 use App\Core\Services\Library\ListAllBooksService;
+use App\Core\Services\Library\ListAllLoansService;
 use App\Core\Services\Library\ListBooksByAuthorService;
 use App\Http\Controllers\CreateAuthorController;
 use App\Http\Controllers\CreateBookController;
 use App\Http\Controllers\CreateLoanController;
 use App\Http\Controllers\ListAllBooksController;
+use App\Http\Controllers\ListAllLoansController;
 use App\Http\Controllers\ListBooksByAuthorController;
 use App\Infra\Adapters\Persistence\Eloquent\Repositories\AuthorEloquentRepository;
 use App\Infra\Adapters\Persistence\Eloquent\Repositories\BookEloquentRepository;
@@ -102,6 +106,15 @@ class AppServiceProvider extends ServiceProvider
             ->give(
                 fn($app) => $app->make(CreateLoanService::class, [
                     "output" => $app->make(CreateLoanJsonPresenter::class),
+                ]),
+            );
+
+        $this->app
+            ->when(ListAllLoansController::class)
+            ->needs(ListAllLoansUseCase::class)
+            ->give(
+                fn($app) => $app->make(ListAllLoansService::class, [
+                    "output" => $app->make(ListAllLoansJsonPresenter::class),
                 ]),
             );
     }
