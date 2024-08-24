221f3879ea240647a4c32375658c56566bd213a2
Thiago Santa Clara <strovsk@outlook.com>
2024-08-24 08:52:36 -0300

    feat: add create loan service step

---
+++ backend/app/Core/Services/Library/CreateLoanService.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Core\Services\Library;
+
+use App\Core\Common\Ports\ViewModel;
+use App\Core\Domain\Library\Entities\Loan;
+use App\Core\Domain\Library\Exceptions\{LoanMustHaveBookId, LoanMustHaveControlDates, LoanMustHaveRightDateFormat};
+use App\Core\Domain\Library\Ports\Persistence\BookRepository;
+use App\Core\Domain\Library\Ports\Persistence\LoanRepository;
+use App\Core\Domain\Library\Ports\UseCases\CreateLoan\{
+    CreateLoanOutputPort,
+    CreateLoanRequestModel,
+    CreateLoanResponseModel,
+    CreateLoanUseCase,
+};
+use DateTime;
+
+final class CreateLoanService implements CreateLoanUseCase
+{
+    /**
+     * @param CreateBookOutputPort $output
+     */
+    public function __construct(private CreateLoanOutputPort $output, private BookRepository $bookRepository, private LoanRepository $loanRepository)
+    {
+    }
+
+    /**
+     * @param CreateLoanRequestModel $requestModel
+     *
+     * @return ViewModel
+     */
+    public function execute(CreateLoanRequestModel $requestModel): ViewModel
+    {
+        $this->validate($requestModel);
+
+        // TODO must validate if book exists before next steps
+        $book = $this->bookRepository->getBookById($requestModel->getBookId());
+
+        $datetimeFormat = 'Y-m-d';
+        $startLoanDate = Datetime::createFromFormat($datetimeFormat, $requestModel->getStartLoanDate());
+        $endLoanDate = Datetime::createFromFormat($datetimeFormat, $requestModel->getEndLoanDate());
+
+        $loan = new Loan(
+            null,
+            $book,
+            $startLoanDate,
+            $endLoanDate,
+        );
+
+        $loan = $this->loanRepository->createOne($loan);
+
+        return $this->output->present(new CreateLoanResponseModel($loan));
+    }
+
+    /**
+     * @param CreateLoanRequestModel $requestModel
+     *
+     * @return void
+     */
+    private function validate(CreateLoanRequestModel $requestModel): void
+    {
+        if (empty($requestModel->getBookId())) {
+            throw new LoanMustHaveBookId();
+        }
+
+        $hasNoLoanStartDate = empty($requestModel->getStartLoanDate());
+        $hasNoLoanEndDate = empty($requestModel->getEndLoanDate());
+        if ($hasNoLoanStartDate || $hasNoLoanEndDate) {
+            throw new LoanMustHaveControlDates();
+        }
+
+        $hasLoanStartDateRightFormat = preg_match('/^\d{4}-\d{2}-\d{2}$/', $requestModel->getStartLoanDate());
+        if (!$hasLoanStartDateRightFormat) {
+            throw new LoanMustHaveRightDateFormat();
+        }
+
+        $hasLoanEndDateRightFormat = preg_match('/^\d{4}-\d{2}-\d{2}$/', $requestModel->getEndLoanDate());
+        if (!$hasLoanEndDateRightFormat) {
+            throw new LoanMustHaveRightDateFormat();
+        }
+
+        // TODO start loan date cannot be greater than end loan date
+    }
+}
