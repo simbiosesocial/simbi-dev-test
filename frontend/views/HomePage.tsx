50e6180ddfcf9c99957121dde98be9ab6773496e
Thiago Santa Clara <strovsk@outlook.com>
2024-08-24 19:43:10 -0300

    feat: add book creation to main page

--- frontend/views/HomePage.tsx
+++ frontend/views/HomePage.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Paper from "@mui/material/Paper";
@@ -5,14 +7,19 @@ import Grid from "@mui/material/Grid";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import SearchIcon from "@mui/icons-material/Search";
-import { BooksList } from "@/features/components";
+import { BooksList, CreateBookDialog } from "@/features/components";
 import { Box } from "@mui/material";
+import { useState } from "react";
 
 type ViewProps = {
   books: Book[];
 };
 
 export default function HomePageView({ books = [] }: ViewProps) {
+  const [shouldCreateBook, setShouldCreateBook] = useState(false);
+  
+  const handleCreateBookDialogOpen = () => setShouldCreateBook(true);
+
   return (
     <Paper sx={{ maxWidth: 936, margin: "auto", overflow: "hidden" }}>
       <AppBar position="static" color="default" elevation={0} sx={{ borderBottom: "1px solid rgba(0, 0, 0, 0.12)" }}>
@@ -33,7 +40,7 @@ export default function HomePageView({ books = [] }: ViewProps) {
               />
             </Grid>
             <Grid item>
-              <Button variant="contained" sx={{ mr: 1 }}>
+              <Button variant="contained" sx={{ mr: 1 }} onClick={handleCreateBookDialogOpen}>
                 Adicionar um livro
               </Button>
             </Grid>
@@ -43,6 +50,7 @@ export default function HomePageView({ books = [] }: ViewProps) {
       <Box sx={{ my: 5, mx: 2 }}>
         <BooksList books={books} />
       </Box>
+      <CreateBookDialog shouldOpen={shouldCreateBook} openCloseControl={setShouldCreateBook} />
     </Paper>
   );
 }
