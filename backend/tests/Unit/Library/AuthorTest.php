bbc83081457dae1a4516465eb0a762cd9550288d
Thiago Santa Clara <strovsk@outlook.com>
2024-08-24 21:13:34 -0300

    test: implement Author entity tests and its VOs

--- backend/tests/Unit/Library/AuthorTest.php
+++ backend/tests/Unit/Library/AuthorTest.php
@@ -2,19 +2,30 @@
 
 namespace Tests\Unit;
 
-// use App\Core\Domain\Library\Entities\Author;
-// use App\Core\Domain\Library\ValueObjects\AuthorName;
+use App\Core\Domain\Library\Entities\Author;
+use App\Core\Domain\Library\Exceptions\InvalidAuthorName;
+use App\Core\Domain\Library\ValueObjects\AuthorName;
 use PHPUnit\Framework\TestCase;
+use DateTime;
 
 class AuthorTest extends TestCase
 {
     public function testShouldBeAbleToInstantiateAnAuhtor()
     {
-        $this->markTestIncomplete();
+        $name = new AuthorName('John', 'Doe');
+        $createdAt = new DateTime('2023-01-01');
+        $updatedAt = new DateTime('2023-01-02');
+        $author = new Author('1', $name, $createdAt, $updatedAt);
+
+        $this->assertEquals('John', $author->firstName);
+        $this->assertEquals('Doe', $author->lastName);
+        $this->assertEquals($createdAt, $author->createdAt);
+        $this->assertEquals($updatedAt, $author->updatedAt);
     }
 
     public function testShouldThrowInvalidAuthorName()
     {
-        $this->markTestIncomplete();
+        $this->expectException(InvalidAuthorName::class);
+        new AuthorName('John');
     }
 }
