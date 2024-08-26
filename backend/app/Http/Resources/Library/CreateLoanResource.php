d655441613e5738b0e7285a2ad56d59bdf20a647
Lígia <ligiabicalho@gmail.com>
2024-08-26 03:52:00 -0300

    feat: Add BookDetailsResource and CreateLoanResource for API responses

---
+++ backend/app/Http/Resources/Library/CreateLoanResource.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Resources\Library;
+
+use App\Core\Domain\Library\Entities\Loan;
+use DateTimeInterface;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+final class CreateLoanResource extends JsonResource
+{
+    /**
+     * @param Loan $loan
+     */
+    public function __construct(private Loan $loan) {}
+
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     *
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request = null)
+    {
+        return [
+            'id' => $this->loan->id,
+            'loanDate' => $this->loan->loanDate->format(DateTimeInterface::ATOM),
+            'returnDate' => $this->loan->returnDate->format(DateTimeInterface::ATOM),
+            'status' => $this->loan->status,
+            'book' => (new BookDetailsResource($this->loan->book))->resolve(),
+            'createdAt' => $this->loan->createdAt->format(DateTimeInterface::ATOM),
+            'updatedAt' => $this->loan->updatedAt->format(DateTimeInterface::ATOM),
+        ];
+    }
+}
