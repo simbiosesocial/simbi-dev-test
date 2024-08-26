996ae5099b8e73cadf1ef20aa944c59b512d0ed3
Lígia <ligiabicalho@gmail.com>
2024-08-26 08:36:33 -0300

    feat: Add ListAllLoansJsonPresenter for presenting all loans in JSON format

---
+++ backend/app/Http/Resources/Library/ListAllLoansResource.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Resources\Library;
+
+use App\Core\Domain\Library\Entities\Loan;
+use DateTime;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ListAllLoansResource extends JsonResource
+{
+    /**
+     * @param Loan $loan
+     */
+    public function __construct(private Loan $loan)
+    {
+    }
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     *
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request = null)
+    {
+        return [
+            'book' => (new BookDetailsResource($this->loan->book))->resolve(),
+            'loanDate' => $this->loan->loanDate->format(DateTime::ATOM),
+            'returnDate' => $this->loan->returnDate->format(DateTime::ATOM),
+            'status' => $this->loan->status,
+            'renewalCount' => $this->loan->renewalCount,
+            'lastRenewedAt' => $this->loan->lastRenewedAt ? $this->loan->lastRenewedAt->format(DateTime::ATOM) : null,
+            'returnedAt' => $this->loan->returnedAt ? $this->loan->returnedAt->format(DateTime::ATOM) : null,
+            'createdAt' => $this->loan->createdAt->format(DateTime::ATOM),
+            'updatedAt' => $this->loan->updatedAt->format(DateTime::ATOM),
+        ];
+    }
+}
