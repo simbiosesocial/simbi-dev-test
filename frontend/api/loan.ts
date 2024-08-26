51456202246be6787face4aa506a6fba473fb5e0
Lígia <ligiabicalho@gmail.com>
2024-08-26 08:59:11 -0300

    feat: Add getAllLoans API endpoint

--- frontend/api/loan.ts
+++ frontend/api/loan.ts
@@ -10,11 +10,29 @@ export const createLoan = async (bookId: string) => {
       body: JSON.stringify({ bookId }),
     });
     if (!response.ok) {
-      throw new Error("Failed to fetch books", { cause: response.statusText });
+      throw new Error("Failed to create loan", { cause: response.statusText });
     }
     const data = await response.json();
     return data;
   } catch (error) {
     console.error("Error creating loan:", error);
+  } 
+}
+
+export const getAllLoans = async () => {
+  try {
+    const response = await fetch(`${env.API_URL}/api/loans`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (!response.ok) {
+      throw new Error("Failed to fetch loans", { cause: response.statusText });
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching loans:", error);
   } 
 }
