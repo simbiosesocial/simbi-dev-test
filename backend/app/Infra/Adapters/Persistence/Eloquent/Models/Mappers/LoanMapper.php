27737d38288c1489b8d29c711f4258218a80c123
Lígia <ligiabicalho@gmail.com>
2024-08-25 23:54:01 -0300

    feat: Add LoanMapper for mapping between domain and persistence models

---
+++ backend/app/Infra/Adapters/Persistence/Eloquent/Models/Mappers/LoanMapper.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Infra\Adapters\Persistence\Eloquent\Models\Mappers;
+
+use App\Core\Domain\Library\Entities\Loan as DomainLoan;
+use App\Infra\Adapters\Persistence\Eloquent\Models\Loan as EloquentLoan;
+
+final class LoanMapper
+{
+    /**
+     * @param DomainLoan $loan
+     *
+     * @return EloquentLoan
+     */
+    public static function toEloquentModel(DomainLoan $loan): EloquentLoan
+    {
+        return new EloquentLoan([
+            'id' => $loan->id,
+            'book_id' => $loan->bookId,
+            'loan_date' => $loan->loanDate,
+            'return_date' => $loan->returnDate,
+            'returned_at' => $loan->returnedAt,
+            'status' => $loan->status,
+            'renewal_count' => $loan->renewalCount,
+            'last_renewed_at' => $loan->lastRenewedAt,
+            'created_at' => $loan->createdAt,
+            'updated_at' => $loan->updatedAt,
+        ]);
+    }
+
+    /**
+     * @param EloquentLoan $loan
+     * @param bool $withRelations
+     *
+     * @return DomainLoan
+     */
+    public static function toDomainEntity(EloquentLoan $loan, bool $withRelations = true): DomainLoan
+    {
+        $domainLoan = new DomainLoan(
+            id: $loan->id,
+            bookId: $loan->book_id,
+            loanDate: $loan->loan_date,
+            returnDate: $loan->return_date,
+            returnedAt: $loan->returned_at,
+            status: $loan->status,
+            renewalCount: $loan->renewal_count,
+            lastRenewedAt: $loan->last_renewed_at,
+            createdAt: $loan->created_at,
+            updatedAt: $loan->updated_at,
+        );
+
+        if ($withRelations && ! empty($loan->book)) {
+            $domainLoan->addBook(BookMapper::toDomainEntity($loan->book));
+        }
+
+        return $domainLoan;
+    }
+}
