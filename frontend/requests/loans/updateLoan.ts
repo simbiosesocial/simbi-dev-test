7866d37ca03b45d1a576dc256a5d2d00def33845
Lígia <ligiabicalho@gmail.com>
2024-08-29 04:09:26 -0300

    feat: Add createLoan, finalizeLoan, and renewLoan request functions

---
+++ frontend/requests/loans/updateLoan.ts
@@ -0,0 +1,37 @@
+export const finalizeLoan = async (loanId: string): Promise<Partial<Loan> | undefined> => {
+  try {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/loans/${loanId}/finalize`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (!response.ok) {
+      throw new Error("Failed to finalize loan", { cause: response.statusText });
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error finalizing loan:", error);
+  } 
+}
+
+export const renewLoan = async (loanId: string): Promise<Partial<Loan> | undefined> => {
+  try {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/loans/${loanId}/renew`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (!response.ok) {
+      throw new Error("Failed to renew loan", { cause: response.statusText });
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error renewing loan:", error);
+  } 
+
+  
+}
