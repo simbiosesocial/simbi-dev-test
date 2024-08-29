7395335c6ffc9f5d6f4a401b9e5dbd955b4d8f83
Lígia <ligiabicalho@gmail.com>
2024-08-29 04:09:01 -0300

    refactor: Update Book and Loan type declarations to include additional attributes

--- frontend/common/config/env.ts
+++ frontend/common/config/env.ts
@@ -4,6 +4,4 @@ const envSchema = z.object({
   API_URL: z.string(),
 });
 
-export const env = envSchema.parse({
-  API_URL: process.env.API_URL || process.env.NEXT_PUBLIC_API_URL,
-});
+export const env = envSchema.parse(process.env);
