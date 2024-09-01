eb9f4d8141ff15e14273ddd645f2d02ff5e7f9ff
Lígia <ligiabicalho@gmail.com>
2024-09-01 13:52:50 -0300

    test: update assertions in the CreateAuthorUseCase and CreateBookUseCase tests

--- backend/tests/Integration/Library/CreateBookUseCaseTest.php
+++ backend/tests/Integration/Library/CreateBookUseCaseTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Integration\Library;
 
 use App\Adapters\Presenters\Library\CreateBookJsonPresenter;
+use App\Core\Domain\Library\Exceptions\AuthorNotFound;
 use App\Core\Domain\Library\Exceptions\BookMustHaveAnAuthor;
 use App\Core\Domain\Library\Exceptions\BookMustHaveAPublisher;
 use App\Core\Domain\Library\Exceptions\BookMustHaveATitle;
@@ -10,8 +11,10 @@ use App\Core\Domain\Library\Ports\UseCases\CreateBook\CreateBookRequestModel;
 use App\Core\Domain\Library\Ports\UseCases\CreateBook\CreateBookUseCase;
 use App\Core\Services\Library\CreateBookService;
 use App\Infra\Adapters\Persistence\Eloquent\Models\Author;
+use App\Infra\Adapters\Persistence\Eloquent\Repositories\AuthorEloquentRepository;
 use App\Infra\Adapters\Persistence\Eloquent\Repositories\BookEloquentRepository;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Ramsey\Uuid\Uuid;
 use Tests\TestCase;
 
 class CreateBookUseCaseTest extends TestCase
@@ -27,6 +30,7 @@ class CreateBookUseCaseTest extends TestCase
         $this->useCase = new CreateBookService(
             output: new CreateBookJsonPresenter(),
             bookRepository: new BookEloquentRepository(),
+            authorRepository: new AuthorEloquentRepository(),
         );
         $this->author = Author::first() ?? Author::factory()->create();
     }
@@ -43,9 +47,9 @@ class CreateBookUseCaseTest extends TestCase
         $book = $this->useCase->execute($request)->resource->toArray(null);
 
         $this->assertIsString($book['id']);
+        $this->assertEquals("Book-Title", $book['title']);
         $this->assertEquals("Book-Publisher", $book['publisher']);
-        $this->assertEquals("Book-Publisher", $book['publisher']);
-        $this->assertEquals($this->author->id, $book['authorId']);
+        $this->assertEquals($this->author->id, $book['author']['id']);
     }
 
     public function testShouldThrowBookMustHaveATitle()
@@ -56,6 +60,7 @@ class CreateBookUseCaseTest extends TestCase
         ]);
 
         $this->expectException(BookMustHaveATitle::class);
+
         $this->useCase->execute($request);
     }
 
@@ -78,6 +83,19 @@ class CreateBookUseCaseTest extends TestCase
         ]);
 
         $this->expectException(BookMustHaveAnAuthor::class);
+        $this->useCase->execute($request);
+    }
+
+    public function testShouldThrowAuthorNotFound()
+    {
+        $invalidAuthorId = Uuid::uuid4();
+        $request = new CreateBookRequestModel([
+            "title" => "Book-Title",
+            "publisher" => "Book-Publisher",
+            "authorId" => $invalidAuthorId,
+        ]);
+
+        $this->expectException(AuthorNotFound::class);
         $this->useCase->execute($request);
     }
 }
