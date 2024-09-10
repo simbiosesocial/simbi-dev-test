8dde523231405d50f27dd24ea0cad4b989b4dca5
Lígia <ligiabicalho@gmail.com>
2024-09-09 22:39:17 -0300

    refactor: Update BookItem component to use SnackAlert for loan success and error messages

--- frontend/features/components/BookItem/BookItem.component.tsx
+++ frontend/features/components/BookItem/BookItem.component.tsx
@@ -4,24 +4,32 @@ import { useState, type FunctionComponent } from "react";
 import type { BookItemProps } from "./BookItem.interface";
 import { Card, CardContent, CardMedia, CardActions, Button, Typography, Tooltip, Snackbar, Alert } from "@mui/material";
 import { createLoan } from "@/requests/loans/createLoan";
+import SnackAlert from "@/common/components/SnackAlert/SnackAlert.component";
 
 
 export const BookItem: FunctionComponent<BookItemProps> = (book) => {
   const { id, title, coverUrl, isAvailable } = book;
   const [isLoading, setIsLoading] = useState(false);
   const [activatedLoan, setActivatedLoan] = useState<boolean>(!isAvailable);
-  const [openSuccess, setOpenSuccess] = useState(false);
-  const [openError, setOpenError] = useState(false);
+  const [alertSuccess, setAlertSuccess] = useState({ open: false, message: '' });
+  const [alertError, setAlertError] = useState({ open: false, message: '' });
+
 
   const handleLoan = async () => {
     setIsLoading(true);
     try {
       const data = await createLoan(id);
       setActivatedLoan(true);
-      if (data) setOpenSuccess(true);
+      if (data) setAlertSuccess({ 
+        open: true, 
+        message: "Empréstimo realizado! Prazo de 7 dias para devolução." 
+      });
     } catch (error) {
       console.error("Error creating loan:", error);
-      setOpenError(true);
+      setAlertError({ 
+        open: true, 
+        message: "Erro ao realizar empréstimo. Tente novamente."
+      });
     } finally {
       setIsLoading(false);
     }
@@ -52,36 +60,9 @@ export const BookItem: FunctionComponent<BookItemProps> = (book) => {
         </Button>
       </CardActions>
     </Card>
-    <Snackbar 
-      open={openSuccess} 
-      autoHideDuration={3500} 
-      onClose={() => setOpenSuccess(false)}
-      anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
-    >
-      <Alert
-        onClose={() => setOpenSuccess(false)}
-        severity="success"
-        variant="filled"
-        sx={{ width: '100%' }}
-      >
-        Empréstimo realizado! Prazo de 7 dias para devolução.
-      </Alert>
-    </Snackbar>
-    <Snackbar 
-      open={openError} 
-      autoHideDuration={3500} 
-      onClose={() => setOpenError(false)}
-      anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
-    >
-      <Alert
-        onClose={() => setOpenError(false)}
-        severity="error"
-        variant="filled"
-        sx={{ width: '100%' }}
-      >
-        Erro ao realizar empréstimo. Tente novamente.
-      </Alert>
-    </Snackbar>
+
+    <SnackAlert severity="success" state={alertSuccess} setState={setAlertSuccess} />
+    <SnackAlert severity="error" state={alertError} setState={setAlertError} />
     </>
   );
 };
