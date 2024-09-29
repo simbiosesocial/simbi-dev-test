4b883e9110713933e6901d9340e82df0321b5e11
Afonso Oliveira <afonso.oliveiragomes@gmail.com>
2024-09-29 01:55:43 -0300

    feat: add loan creation form and integrate with API

--- frontend/common/config/env.ts
+++ frontend/common/config/env.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 const envSchema = z.object({
-  API_URL: z.string(),
+  NEXT_PUBLIC_API_URL: z.string(),
 });
 
 export const env = envSchema.parse(process.env);
--- frontend/features/components/BookItem/BookItem.component.tsx
+++ frontend/features/components/BookItem/BookItem.component.tsx
@@ -1,8 +1,29 @@
+'use client'
+
 import type { FunctionComponent } from "react";
 import type { BookItemProps } from "./BookItem.interface";
 import { Card, CardContent, CardMedia, CardActions, Button, Typography, Tooltip } from "@mui/material";
 
-export const BookItem: FunctionComponent<BookItemProps> = ({ title, coverUrl }) => {
+export const BookItem: FunctionComponent<BookItemProps> = ({ id, title, coverUrl }) => {
+  const loanBook = async () => {
+    console.log(process.env)
+    try {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/loans/book/${id}`, {
+        method: 'POST',
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to loan book", { cause: response.statusText });
+      }
+
+      await response.json();
+
+      alert('Empréstimo feito com sucesso')
+    } catch {
+      alert('Erro ao pegar livro emprestado')
+    }
+  }
+
   return (
     <Card variant="outlined">
       <CardMedia sx={{ height: 264 }} image={coverUrl ? coverUrl : "/cover.png"} title={title} />
@@ -17,7 +38,7 @@ export const BookItem: FunctionComponent<BookItemProps> = ({ title, coverUrl }) 
         </Typography>
       </CardContent>
       <CardActions>
-        <Button size="small" variant="contained" fullWidth>
+        <Button size="small" variant="contained" fullWidth onClick={loanBook}>
           Empréstimo
         </Button>
       </CardActions>
